06cb4d4af66583484d50f54bc4d0016c728a10e9
daniel carvalho <dnc331@gmail.com>
2022-12-27 05:38:10 -0300

    refactor to use moment js

--- oasis.js
+++ oasis.js
@@ -1,12 +1,35 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    chrome.storage.local.get("workedDaysData", ({ workedDaysData }) => {
+    chrome.storage.local.get("workedDaysData", ({workedDaysData}) => {
         updateTable(workedDaysData)
     });
 });
 
 function updateTable(tableData) {
-    tableData.forEach((data, idx) => {
-        document.getElementById(`day-${idx+1}-header`).textContent = data.weekdayText
-        document.getElementById(`day-${idx+1}-data`).textContent = data.workedHours
+    const wrapper = document.getElementById("today-hours-worked-info")
+    const loadingWrapper = document.getElementById("loading-state")
+    const tbody = document.getElementById("oasis__table__body")
+    if (tableData.length) {
+        wrapper.classList.toggle("d-none")
+        loadingWrapper.classList.toggle("d-none")
+    }
+    tableData.forEach(({hoursWorked, hoursRemaining, date}, idx) => {
+        const row = document.createElement("tr")
+        const remaining = document.createElement("td")
+        const today = document.createElement("td")
+        const worked = document.createElement("td")
+
+        today.append(date.weekDay + " - " + date.date)
+        worked.append(hoursWorked)
+        remaining.append(hoursRemaining)
+
+
+        row.appendChild(today)
+        row.appendChild(worked)
+        row.appendChild(remaining)
+        tbody.appendChild(row)
+
+
     });
+
+
 }
